cb98e2a99772d122cd5954364b6b955b3a921ca3
arunprasadbh <arunprasad_bh@yahoo.com>
2017-08-13 22:38:11 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,12 @@ console.log('Loaded!');
 
 var imgelement = document.getElementById('madi');
 
+var marginLeft = 0;
+
 function moveRight(){
     marginLeft = marginLeft + 10;
     imgelement.style.marginLeft = marginLeft + 'px';
-};
+}
 
 imgelement.onclick = function () {
     var interval = setInterval(moveRight, 100);
