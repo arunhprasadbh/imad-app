cc925846b8d7a0dfc9f54978b1bb89139dbaa94c
arunprasadbh <arunprasad_bh@yahoo.com>
2017-08-15 23:07:51 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/:articleName', function (req, res){
 });
 
 var names = [];
-app.get('/submit_name/?name', function(req, res){
+app.get('/submit_name', function(req, res){
    //Get the name
    var name = req.query.name;
    //Add name to variable
