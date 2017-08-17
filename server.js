1c25a20b5adf61320d38caf7b25a1d27a91b3741
arunprasadbh <arunprasad_bh@yahoo.com>
2017-08-17 22:10:19 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ var pool = new Pool(config);
 var app = express();
 app.use(morgan('combined'));
 
+//Test Db Connection
+app.get('/test-db1', function(req, res){
+    console.log('iam in test db1');
+    pool.query('select * from test1', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringfy(result));
+        }
+    });
+    
+});
+
 var articles = {
      'article-one' : {
         title: 'Article One | Aruna Bhamidipati',
@@ -99,18 +112,7 @@ function createTemplate(data){
 }
 var counter = 0; 
 
-//Test Db Connection
-app.get('/test-db1', function(req, res){
-    console.log('iam in test db1');
-    pool.query('select * from test1', function(err, result){
-        if(err){
-            res.status(500).send(err.toString());
-        }else{
-            res.send(JSON.stringfy(result));
-        }
-    });
-    
-});
+
 
 app.get('/counter', function(req, res){
    counter = counter + 1; 
