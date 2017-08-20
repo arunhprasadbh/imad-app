272f1358fc3d1c0af79427be4e0fef4ace5af84f
arunprasadbh <arunprasad_bh@yahoo.com>
2017-08-20 10:51:07 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ var counter = 0;
 function hash(input, salt){
     console.log('check 2.1');
     //var hashString = salt;
-    var hashString = crypto.pbkdf2Sync(input, salt, 100, 512, 'sha512');
+    var hashString = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     console.log('check 2.2');
     
     return hashString;
